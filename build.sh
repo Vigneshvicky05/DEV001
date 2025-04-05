12557693a8e12a0ef89be187c45ec5496d56837e
Vignesh05 <135207642+Vigneshvicky05@users.noreply.github.com>
2025-04-05 09:39:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 docker build -t task37 .
 echo Hyyy
-docker login -u deepan47 -p Deepan11032004
-docker tag task37 deepan47/finaltask
-docker push deepan47/finaltask
+docker login -u vigneshwaran1806 -p Vignesh#2005
+docker tag task37 vigneshwaran1806/finaltask
+docker push vigneshwaran1806/finaltask
 kubectl apply -f  deploy.yaml --validate=false
 kubectl apply -f svc.yaml --validate=false
+
+
